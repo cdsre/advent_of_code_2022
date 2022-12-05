06d896056322a0d10cfb66fbfc0a92cb4c9e7274
Chris Doyle <uk.chris.doyle@gmail.com>
2022-12-05 07:20:34 +0000

    day 5 part 1

--- src/day_5.py
+++ src/day_5.py
@@ -1,2 +1,62 @@
-def get_initial_stacks(crane_procedure):
-    pass
+import re
+
+
+def get_initial_stacks(crane_procedure: list) -> dict[list]:
+    # capture the stacks only
+    stacks = []
+    for line in crane_procedure:
+        if line == "":
+            break
+        stacks.append(line)
+
+    # transpose the stacks and pick only those that start with a number
+    trans_stack = list(zip(*stacks[::-1]))
+    stacks = {s[0]: list(s[1:]) for s in zip(*stacks[::-1]) if s[0].isdigit()}
+
+    # pop any empty items in the stack
+    for stack in stacks.values():
+        while stack[-1] == " ":
+            stack.pop()
+
+    return stacks
+
+
+def parse_move_instruction(move_instruction: str) -> list:
+    match = re.findall(r"(\d+)", move_instruction)
+    return match
+
+
+def get_move_instructions(crane_procedure: list) -> list:
+    split_point = crane_procedure.index("")
+    return crane_procedure[split_point + 1:]
+
+
+def process_move_instruction(stacks: dict[str, list], num_crates, from_stack, to_stack) -> None:
+    for _ in range(int(num_crates)):
+        stacks[to_stack].append(stacks[from_stack].pop())
+
+
+def execute_crane_procedure(stacks, move_instructions):
+    for move in move_instructions:
+        num_crates, from_stack, to_stack = parse_move_instruction(move)
+        process_move_instruction(stacks, num_crates, from_stack, to_stack)
+
+
+def get_top_of_stacks(stacks):
+    return [stack[-1] for stack in stacks.values()]
+
+
+def rearrange_and_get_top_stacks(crane_procedure):
+    stacks = get_initial_stacks(crane_procedure)
+    moves = get_move_instructions(crane_procedure)
+    execute_crane_procedure(stacks, moves)
+    return "".join(get_top_of_stacks(stacks))
+
+def main():
+    with open("../input/day_5.txt") as input_file:
+        crane_procedure = input_file.read().splitlines()
+        top_after_rearrange = rearrange_and_get_top_stacks(crane_procedure)
+        print(f"{top_after_rearrange=}")
+
+if __name__ == "__main__":
+    main()
--- test/test_day_5.py
+++ test/test_day_5.py
@@ -1,4 +1,6 @@
 from pytest import fixture
+from src.day_5 import get_initial_stacks, parse_move_instruction, get_move_instructions, process_move_instruction, \
+    execute_crane_procedure, get_top_of_stacks, rearrange_and_get_top_stacks
 
 
 @fixture()
@@ -14,3 +16,41 @@ move 2 from 2 to 1
 move 1 from 1 to 2""".splitlines()
 
 
+@fixture(scope="function")
+def stack_map():
+    return {'1': ['Z', 'N'], '2': ['M', 'C', 'D'], '3': ['P']}
+
+
+@fixture(scope="function")
+def move_instructions():
+    return ['move 1 from 2 to 1', 'move 3 from 1 to 3', 'move 2 from 2 to 1', 'move 1 from 1 to 2']
+
+
+def test_get_initial_stacks(crane_procedure, stack_map):
+    assert get_initial_stacks(crane_procedure) == stack_map
+
+
+def test_parse_move_instruction():
+    assert parse_move_instruction("move 1 from 2 to 1") == ['1', '2', '1']
+
+
+def test_get_move_instructions(crane_procedure, move_instructions):
+    assert get_move_instructions(crane_procedure) == move_instructions
+
+
+def test_process_move_instruction(stack_map):
+    process_move_instruction(stack_map, "1", "2", "3")
+    assert stack_map == {'1': ['Z', 'N'], '2': ['M', 'C'], '3': ['P', 'D']}
+
+
+def test_execute_crane_procedure(stack_map, move_instructions):
+    execute_crane_procedure(stack_map, move_instructions)
+    assert stack_map == {'1': ['C'], '2': ['M'], '3': ['P', 'D', 'N', 'Z']}
+
+
+def test_get_top_of_stacks(stack_map):
+    assert get_top_of_stacks(stack_map) == ['N', 'D', 'P']
+
+
+def test_rearrange_and_get_top_stacks(crane_procedure):
+    assert rearrange_and_get_top_stacks(crane_procedure) == "CMZ"
